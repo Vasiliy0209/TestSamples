6020bf5f0cdf4e3ebc1aa69df17c8fcc03e513e9
SU-Nesterenko <55326271+SU-Nesterenko@users.noreply.github.com>
2022-02-25 12:24:56 +0500

    Добавлены примеры программного кода автоматизированных тестов

---
+++ MyClassLibrary/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+
+    [Serializable]
+    public class MyDivideException : Exception
+    {
+        public MyDivideException() { }
+        public MyDivideException(string message) : base(message) { }
+        public MyDivideException(string message, Exception inner) : base(message, inner) { }
+        protected MyDivideException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+    public class MyCalc
+    {
+        public static double Divide(double a, double b)
+        {
+           /* if (b == 0)
+                throw new MyDivideException("Деление на 0");*/
+
+            return a / b;
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b +1;
+        }
+
+    }
+}
---
+++ MyClassLibrary/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyUnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSum10and5()
+        {
+            double result = MyClassLibrary.MyCalc.Sum(10, 5);
+
+            //Сравниваем ожидаемое и фактическое значение
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod]
+        public void TestDivide10on5()
+        {
+            double result = MyClassLibrary.MyCalc.Divide(10,5);
+
+            //Сравниваем ожидаемое и фактическое значение
+            Assert.AreEqual(2,result);
+        }
+
+        [TestMethod]
+        public void TestDivide10on0()
+        {
+            try
+            {
+                double result = MyClassLibrary.MyCalc.Divide(10, 0);
+            }catch(MyClassLibrary.MyDivideException ex)
+            {
+                return;
+            }catch(Exception ex)
+            {
+                Assert.Fail("Сгенерировано неизвестное исключение");
+            }
+            Assert.Fail("Исключение не сгенерировано");            
+        }
+    }
+}
